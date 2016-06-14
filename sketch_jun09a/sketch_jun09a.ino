5bd77e87731d81dfc5c5390e8e1b41516705d193
Pedro Cunial <pedrocc4@al.insper.edu.br>
2016-06-14 11:31:36 -0300

    codigo da aula

--- sketch_jun09a/sketch_jun09a.ino
+++ sketch_jun09a/sketch_jun09a.ino
@@ -1,25 +1,22 @@
 #define BUFF_SIZE 40
 char buffer[BUFF_SIZE];
 
-void setup(){
-  Serial.begin(57600);
-  Serial1.begin(57600);
-  Serial1.write("#o1");  
-  Serial.println("Starting Communication with Razor IMU");
+void setup() {
+    Serial.begin(57600);
+    Serial.println("Starting Communication with Razor IMU");
 }
 
 void loop() {
-  Serial1.write("#o1");
-  int i = 0;
-  while(Serial1.available()) {
-    buffer[i++] = Serial1.read();
-    Serial.println(buffer[i]);
-    if(i == BUFF_SIZE) 
-      break;
-  }
+    int i = 0;
+    while(Serial.available()) {
+        buffer[i++] = Serial.read();
+        // Serial.println(buffer[i]);
+        if(i == BUFF_SIZE)
+            break;
+    }
 
-  for(int j = 0; j < i; j++) {
-    Serial.print(buffer[j]);
-  }
-
+    for(int j = 0; j < i; j++) {
+        Serial.print(buffer[j]);
+    }
+    Serial.print('\n');
 }
