ac0362c397e01504c7705ff3c6817b879a0c8d7c
Pedro Cunial <pedrocc4@al.insper.edu.br>
2016-06-14 11:39:21 -0300

    codigo da aula

--- sketch_jun09a/sketch_jun09a.ino
+++ sketch_jun09a/sketch_jun09a.ino
@@ -7,16 +7,27 @@ void setup() {
 }
 
 void loop() {
+
     int i = 0;
     while(Serial.available()) {
+        Serial.println("Teste");
         buffer[i++] = Serial.read();
-        // Serial.println(buffer[i]);
+
+        // Print it out in many formats:
+        Serial.println(buffer[i]);       // print as an ASCII-encoded decimal
+        Serial.println(buffer[i], DEC);  // print as an ASCII-encoded decimal
+        Serial.println(buffer[i], HEX);  // print as an ASCII-encoded hexadecimal
+        Serial.println(buffer[i], OCT);  // print as an ASCII-encoded octal
+        Serial.println(buffer[i], BIN);  // print as an ASCII-encoded binary
+        // Serial.println(buffer[i], BYTE); // print as a raw byte value
+
         if(i == BUFF_SIZE)
             break;
     }
 
-    for(int j = 0; j < i; j++) {
-        Serial.print(buffer[j]);
-    }
-    Serial.print('\n');
+    // for(int j = 0; j < i; j++) {
+    //     Serial.print(buffer[j]);
+    //
+    // }
+
 }
