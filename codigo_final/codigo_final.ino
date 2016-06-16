51cd8a0d16d4887f4173911e7872a3019a6410e2
MarceloHarad <marcelogdeandrade@gmail.com>
2016-06-16 15:30:02 -0300

    robot almost working

--- codigo_final/codigo_final.ino
+++ codigo_final/codigo_final.ino
@@ -1,4 +1,4 @@
-#define BUFF_SIZE 100
+#define BUFF_SIZE 40
 
 #define DigitalRight 10
 #define DigitalLeft 11
@@ -6,7 +6,12 @@
 #define PWMRight 32
 #define PWMLeft 33
 
+
 char buffer[BUFF_SIZE];
+String list;
+String num1;
+String num2;
+String num3;
 
 void setup(){
 
@@ -15,26 +20,44 @@ void setup(){
 
   pinMode(PWMRight, OUTPUT);
   pinMode(PWMLeft, OUTPUT);
-  
+
   Serial.begin(57600);
   Serial1.begin(57600);
-  Serial1.write("#o1");  
+  Serial1.write("#o1");
 
-  
+
   Serial.println("Starting Communication with Razor IMU");
 }
 
 void loop() {
 
-  
+
   int i = 0;
   while(Serial1.available()) {
-    buffer[i++] = Serial1.read();
-    if(i == BUFF_SIZE) 
+      digitalWrite(DigitalRight,HIGH);
+      digitalWrite(DigitalLeft,HIGH);
+      list = Serial1.readStringUntil('#');
+      list = list.substring(4);
+      int comma1 = list.indexOf(',');
+      int comma2 = list.indexOf(',',comma1 + 1);
+      num1 = list.substring(0,comma1);
+      num2 = list.substring(comma1 + 1,comma2);
+      num3 = list.substring(comma2 + 1);
+
+      float ang = num3.toFloat();
+      Serial.print(ang);
+      if (ang < 140 && ang > -140){
+          analogWrite(PWMRight,255);
+          analogWrite(PWMLeft,-255);
+      }
+      else {
+          analogWrite(PWMRight,-255);
+          analogWrite(PWMLeft,255);
+      }
+
+      Serial.println(num2);
+    if(i == BUFF_SIZE)
       break;
   }
-  for(int j = 0; j < i; j++) {
-    Serial.print(buffer[j]);
-    Serial.print("|");
-  }
+
 }
